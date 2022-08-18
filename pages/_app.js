0e1af23e83430fff7d3c23b80bfadb12dce803f0
Kristian Jackson <kristian.jackson@gmail.com>
2022-08-18 13:25:06 +0000

    added some login base work

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,25 @@
 // _app.js
 
+import { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import "../styles/globals.css";
+import { supabase } from '../utils/supabase';
 
 function MyApp({ Component, pageProps }) {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+}, []);
+
   return (
-    <div className="container">
-      <Navbar />
-      <Component {...pageProps} />
+    <div>
+      <Navbar session={session} />
+      <Component {...pageProps} session={session} />
       <Footer />
     </div>
   );
